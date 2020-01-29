ffd05ca925468a56b13e07b04e586fdf9bd56acc
charlie <charlie@maddex.co>
2020-01-29 22:32:59 +0000

    Created arg handler
    Grabs domain/username/password from arguments and assigned them to a static string.

--- src/DomainJoiner/Program.cs
+++ src/DomainJoiner/Program.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace DomainJoiner {
     class Program {
+
+        private static string domain, username, password;
+
         static void Main(string[] args) {
+            
 
             if (args.Length == 0) {
                 /* Ask user to input params */
@@ -16,8 +20,15 @@ namespace DomainJoiner {
                 /* loop args array */
                 for (int i = 0; i < args.Length; i++) {
                     Console.Write(args[i] + Environment.NewLine);
-
+                    if (i == 0)
+                        domain = args[i];
+                    if (i == 1)
+                        username = args[i];
+                    if (i == 2)
+                        password = args[i];
                 }
+
+                Console.WriteLine($"Domain: {domain} | Username: {username} | Password: {password}");
                 
                 /* keep window open for debugging */
                 Console.Read();
