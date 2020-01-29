23b7b5e972380523a504f213b01aa177ddc4efae
charlie <charlie@maddex.co>
2020-01-29 22:40:17 +0000

    Domain Join Method

--- src/DomainJoiner/Program.cs
+++ src/DomainJoiner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,7 @@ namespace DomainJoiner {
                 System.Console.WriteLine("Please enter parameter values.");
                 Console.Read();
             } else if (args.Length > 3) {
+                /* Too many params */
                 System.Console.WriteLine("Arguments overloaded.");
                 Console.Read();
             } else {
@@ -31,11 +33,39 @@ namespace DomainJoiner {
                 }
 
                 Console.WriteLine($"Domain: {domain} | Username: {username} | Password: {password}");
+
+
                 
                 /* keep window open for debugging */
                 Console.Read();
             }
 
+        }
+
+        public static bool Connect(string domain, string username, string password) {
+            using (ManagementObject wmiObject = new ManagementObject(new ManagementPath("Win32_ComputerSystem.Name='" + Environment.MachineName + "'"))) {
+                try {
+                    // Obtain in-parameters for the method
+                    ManagementBaseObject inParams = wmiObject.GetMethodParameters("JoinDomainOrWorkgroup");
+                    inParams["Name"] = domain;
+                    inParams["Password"] = password;
+                    inParams["UserName"] = username;
+                    inParams["FJoinOptions"] = 3; // Magic number: 3 = join to domain and create computer account
+                    // Execute the method and obtain the return values.
+                    ManagementBaseObject joinParams = wmiObject.InvokeMethod("JoinDomainOrWorkgroup", inParams, null);
+
+                    // Did it work?
+                    if ((uint)(joinParams.Properties["ReturnValue"].Value) != 0) {
+                        Console.WriteLine(string.Format("JoinDomainOrWorkgroup return code: '{0}'", joinParams["ReturnValue"]));
+                        // Join to domain didn't work
+                        Console.WriteLine(string.Format("JoinDomainOrWorkgroup failed with return code: '{0}', if '1355' there is no connection to the domain.", joinParams["ReturnValue"]));
+                        return false;
+                    }
+                    return false;
+                } catch {
+                    return false;
+                }
+            }
         }
     }
 }
