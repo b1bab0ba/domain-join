74a3be026b13f6496ff5ea16a6b87433047f1ce7
charlie <charlie@maddex.co>
2020-01-29 22:34:59 +0000

    Arg error handling

--- src/DomainJoiner/Program.cs
+++ src/DomainJoiner/Program.cs
@@ -10,16 +10,18 @@ namespace DomainJoiner {
         private static string domain, username, password;
 
         static void Main(string[] args) {
-            
+
 
             if (args.Length == 0) {
                 /* Ask user to input params */
                 System.Console.WriteLine("Please enter parameter values.");
                 Console.Read();
+            } else if (args.Length > 3) {
+                System.Console.WriteLine("Arguments overloaded.");
+                Console.Read();
             } else {
                 /* loop args array */
                 for (int i = 0; i < args.Length; i++) {
-                    Console.Write(args[i] + Environment.NewLine);
                     if (i == 0)
                         domain = args[i];
                     if (i == 1)
