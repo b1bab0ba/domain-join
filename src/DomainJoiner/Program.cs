174ffc5fbab58887d15dc757dcca570ac4091c03
charlie <charlie@maddex.co>
2020-01-29 22:28:10 +0000

    Arg handling

--- src/DomainJoiner/Program.cs
+++ src/DomainJoiner/Program.cs
@@ -8,6 +8,20 @@ namespace DomainJoiner {
     class Program {
         static void Main(string[] args) {
 
+            if (args.Length == 0) {
+                /* Ask user to input params */
+                System.Console.WriteLine("Please enter parameter values.");
+                Console.Read();
+            } else {
+                /* loop args array */
+                for (int i = 0; i < args.Length; i++) {
+                    Console.Write(args[i] + Environment.NewLine);
+
+                }
+                
+                /* keep window open for debugging */
+                Console.Read();
+            }
 
         }
     }
